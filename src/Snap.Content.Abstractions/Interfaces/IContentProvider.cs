ad2fc3f5fae49d299489675beaa465ac657ac9d1
Shmellyorc <shmellyorc@gmail.com>
2026-04-15 11:15:19 -0700

    Old Packages not needed anymore. They were suppose to be removed from the previous commit.

--- src/Snap.Content.Abstractions/Interfaces/IContentProvider.cs
+++
@@ -1,8 +0,0 @@
-namespace Snap.Content.Abstractions.Interfaces;
-
-public interface IContentProvider
-{
-	bool Exists(string path);
-	Stream OpenRead(string path);
-	IEnumerable<string> List(string folder);
-}
